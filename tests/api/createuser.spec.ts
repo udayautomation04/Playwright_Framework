de0758ec3e6713d39c94618521136953f8c876fa
udayautomation04 <udayjetset@gmail.com>
2026-07-18 22:49:06 +0530

    Print Token

--- tests/api/createuser.spec.ts
+++ tests/api/createuser.spec.ts
@@ -6,16 +6,17 @@ import { test, expect } from "../../src/fixtures/apifixtures";
 const TOKEN = process.env.APITOKEN!;
 let AUTH_HEADER = { Authorization: `Bearer ${TOKEN}` };
 let userId: number;
-test.describe.serial('@smoke @regression @running e2e go rest crud api tests', () => {
+test.describe.serial(' @smoke @running e2e go rest crud api tests', () => {
 
-test('GET API -- get all users', async ({ apiUtility }) => {
+test('@P1 GET API -- get all users', async ({ apiUtility }) => {
     let response = await apiUtility.get('/public/v2/users', AUTH_HEADER);
+    console.log("Token is ",TOKEN);
     expect(response.status).toBe(200)
     expect(response.body.length).toBeGreaterThan(0);
     console.log(response.body)
 })
 
-test('Post API--Create user', async ({ apiUtility }) => {
+test('@P2 Post API--Create user', async ({ apiUtility }) => {
 
     let userData = {
         name: 'uday101',
@@ -23,6 +24,7 @@ test('Post API--Create user', async ({ apiUtility }) => {
         gender: 'male',
         status: 'active'
     };
+    console.log("Token is ",TOKEN);
     console.log('auth:', AUTH_HEADER)
     let response = await apiUtility.post('/public/v2/users', userData, AUTH_HEADER);
     expect(response.status).toBe(201);
@@ -31,19 +33,20 @@ test('Post API--Create user', async ({ apiUtility }) => {
     console.log('Created User Id:', userId)
 })
 
-test('PUT API -- Update a user', async ({ apiUtility }) => {
+test('@P3 PUT API -- Update a user', async ({ apiUtility }) => {
         let userUpdatedData = {
             name: 'Update',
             status: 'inactive'
         };
 
         let response = await apiUtility.put(`/public/v2/users/${userId}`, userUpdatedData, AUTH_HEADER);
+        console.log("Token is ",TOKEN);
         expect(response.status).toBe(200);
         expect(response.body.name).toBe(userUpdatedData.name);
         expect(response.body.status).toBe(userUpdatedData.status);
     });
 
-    test('DELETE API -- Delete a user', async ({ apiUtility }) => {
+    test('@P4 DELETE API -- Delete a user', async ({ apiUtility }) => {
         let response = await apiUtility.delete(`/public/v2/users/${userId}`, AUTH_HEADER);
         expect(response.status).toBe(204);
     });
